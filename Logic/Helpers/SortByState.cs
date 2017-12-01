a3470ad7edc87aeedfccc2b8ec83ddcb5805b3cc
Artem <artemilyin2013@gmail.com>
2017-12-01 22:18:08 +0300

    Реализован SortByState

--- Logic/Helpers/SortByState.cs
+++ Logic/Helpers/SortByState.cs
@@ -1,18 +1,14 @@
-﻿using coursesProject.Data;
-using coursesProject.Models;
-using System;
+﻿using coursesProject.Models;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace coursesProject.Helpers
+namespace Logic.Helpers
 {
     public static class SortByState
     {
-        public static List<UserViewModel> UsersSort(this  List<UserViewModel> users, string Sort)
+        public static List<UserViewModel> UsersSort(this List<UserViewModel> users, string Sort)
         {
             List<UserViewModel> Users = new List<UserViewModel>();
-            if (Sort=="all")
+            if (Sort == "all")
             {
                 return users;
             }
